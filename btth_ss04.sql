2bc913b7e6dfcfa9021aa0e6cd9130613dcebb43
cout "DangVanThangendl <vthang041206@gmail.com>
2025-12-30 14:05:15 +0700

    Add files via upload

---
+++ btth_ss04.sql
@@ -0,0 +1,60 @@
+create database btth_ss04;
+use btth_ss04;
+create table Reader(
+	reader_id int primary key auto_increment,
+    reader_name varchar(100) not null,
+    phone varchar(15) unique,
+    register_date date default(current_date())
+);
+create table Book(
+	book_id int primary key,
+    book_title varchar(150) not null, 
+    author varchar(100),
+    publish_year int check(publish_year >= 1900)
+);
+create table Borrow(
+	reader_id int,
+    book_id int,
+    borrow_date date default(current_date()),
+    return_date date,
+	foreign key (reader_id) references Readers(reader_id),
+    foreign key (book_id) references Books(book_id)
+);
+
+alter table Reader add email varchar(100) unique;
+alter table Book modify author varchar(150);
+alter table Borrow add check (return_date >= borrow_date);
+
+insert into Reader(reader_id,reader_name,phone,email,register_date)
+values 
+(1,'NguyenVanan','0901234567','an.nguyen@gmail.com','2024-09-01'),
+(2,'NguyenVanB','0912345678','binh.tran@gmail.com','2024-09-05'),
+(3,'NguyenVanC','0923456789','chau.le@gmail.com','2024-09-10');
+
+insert into Book(book_id,book_title,author,publish_year)
+values
+(101,'Lập trình C căn bản','Nguyễn Văn A',2018),
+(102,'Cơ sở dữ liệu','Trần Thị B',2020),
+(103,'Lập trình Java','Lê Minh C',2019),
+(104,'Hệ quản trị MySQL','Phạm Văn D',2021);
+
+insert into Borrow(reader_id,book_id,borrow_date,return_date)
+values
+(1,101,'2024-09-15',NULL),
+(1,102,'2024-09-15','2024-09-25'),
+(2,103,'2024-09-18',NULL);
+
+update Borrow
+set return_date = '2024-10-01'
+where reader_id =1;
+
+update Book 
+set publish_year = 2023
+where publish_year >= 2021;
+
+delete from Borrow
+where borrow_date < '2024-09-18';
+
+select * from Book;
+select * from Borrow;
+select * from Reader;
